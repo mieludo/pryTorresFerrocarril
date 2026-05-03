81cae6d00d29383c64ec6b72f3db92c888be7561
Joaquin Torres <joaquintttorres@hotmail.com>
2026-05-03 18:04:19 -0300

    cambio de interfaz

--- pryTorresFerrocarril/frmFerro.Designer.cs
+++ pryTorresFerrocarril/frmFerro.Designer.cs
@@ -33,24 +33,23 @@
             this.lblTitulo = new System.Windows.Forms.Label();
             this.lblSubtitulo = new System.Windows.Forms.Label();
             this.gbDatos = new System.Windows.Forms.GroupBox();
-            this.lblDatos = new System.Windows.Forms.Label();
-            this.lblDistancia = new System.Windows.Forms.Label();
-            this.lblDias = new System.Windows.Forms.Label();
+            this.mtbDias = new System.Windows.Forms.MaskedTextBox();
             this.mtbDistancia = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.lblDatos = new System.Windows.Forms.Label();
             this.gbInformacion = new System.Windows.Forms.GroupBox();
-            this.lblInformacion = new System.Windows.Forms.Label();
             this.lblPrecio = new System.Windows.Forms.Label();
-            this.lblDescuento = new System.Windows.Forms.Label();
-            this.lblCondicion = new System.Windows.Forms.Label();
-            this.lblEstancia = new System.Windows.Forms.Label();
-            this.lblBoleto = new System.Windows.Forms.Label();
-            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblInformacion = new System.Windows.Forms.Label();
             this.btnLimpiar = new System.Windows.Forms.Button();
             this.btnCalcular = new System.Windows.Forms.Button();
+            this.gbCondicionDescuento = new System.Windows.Forms.GroupBox();
+            this.lblCondicionDescuento = new System.Windows.Forms.Label();
+            this.lblInformacionDescuento = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbTren)).BeginInit();
             this.gbDatos.SuspendLayout();
             this.gbInformacion.SuspendLayout();
+            this.gbCondicionDescuento.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbTren
@@ -90,16 +89,53 @@
             // 
             // gbDatos
             // 
-            this.gbDatos.Controls.Add(this.maskedTextBox1);
+            this.gbDatos.Controls.Add(this.mtbDias);
             this.gbDatos.Controls.Add(this.mtbDistancia);
             this.gbDatos.Controls.Add(this.lblDias);
             this.gbDatos.Controls.Add(this.lblDistancia);
             this.gbDatos.Controls.Add(this.lblDatos);
-            this.gbDatos.Location = new System.Drawing.Point(-1, 65);
+            this.gbDatos.Location = new System.Drawing.Point(78, 74);
             this.gbDatos.Name = "gbDatos";
             this.gbDatos.Size = new System.Drawing.Size(339, 149);
             this.gbDatos.TabIndex = 3;
             this.gbDatos.TabStop = false;
+            // 
+            // mtbDias
+            // 
+            this.mtbDias.Location = new System.Drawing.Point(161, 101);
+            this.mtbDias.Mask = "99";
+            this.mtbDias.Name = "mtbDias";
+            this.mtbDias.Size = new System.Drawing.Size(91, 20);
+            this.mtbDias.TabIndex = 4;
+            // 
+            // mtbDistancia
+            // 
+            this.mtbDistancia.Location = new System.Drawing.Point(161, 60);
+            this.mtbDistancia.Mask = "99999";
+            this.mtbDistancia.Name = "mtbDistancia";
+            this.mtbDistancia.Size = new System.Drawing.Size(91, 20);
+            this.mtbDistancia.TabIndex = 3;
+            this.mtbDistancia.ValidatingType = typeof(int);
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDias.Location = new System.Drawing.Point(14, 105);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(116, 16);
+            this.lblDias.TabIndex = 2;
+            this.lblDias.Text = "Días de estancia:";
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDistancia.Location = new System.Drawing.Point(14, 64);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(101, 16);
+            this.lblDistancia.TabIndex = 1;
+            this.lblDistancia.Text = "Distancia (km):";
             // 
             // lblDatos
             // 
@@ -112,57 +148,24 @@
             this.lblDatos.TabIndex = 0;
             this.lblDatos.Text = "📖 Datos del viaje";
             // 
-            // lblDistancia
-            // 
-            this.lblDistancia.AutoSize = true;
-            this.lblDistancia.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDistancia.Location = new System.Drawing.Point(14, 64);
-            this.lblDistancia.Name = "lblDistancia";
-            this.lblDistancia.Size = new System.Drawing.Size(101, 16);
-            this.lblDistancia.TabIndex = 1;
-            this.lblDistancia.Text = "Distancia (km):";
-            // 
-            // lblDias
-            // 
-            this.lblDias.AutoSize = true;
-            this.lblDias.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDias.Location = new System.Drawing.Point(14, 105);
-            this.lblDias.Name = "lblDias";
-            this.lblDias.Size = new System.Drawing.Size(116, 16);
-            this.lblDias.TabIndex = 2;
-            this.lblDias.Text = "Días de estancia:";
-            // 
-            // mtbDistancia
-            // 
-            this.mtbDistancia.Location = new System.Drawing.Point(161, 60);
-            this.mtbDistancia.Mask = "99999";
-            this.mtbDistancia.Name = "mtbDistancia";
-            this.mtbDistancia.Size = new System.Drawing.Size(91, 20);
-            this.mtbDistancia.TabIndex = 3;
-            this.mtbDistancia.ValidatingType = typeof(int);
-            // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(161, 101);
-            this.maskedTextBox1.Mask = "99";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(91, 20);
-            this.maskedTextBox1.TabIndex = 4;
-            // 
             // gbInformacion
             // 
-            this.gbInformacion.Controls.Add(this.lblResultado);
-            this.gbInformacion.Controls.Add(this.lblBoleto);
-            this.gbInformacion.Controls.Add(this.lblEstancia);
-            this.gbInformacion.Controls.Add(this.lblCondicion);
-            this.gbInformacion.Controls.Add(this.lblDescuento);
             this.gbInformacion.Controls.Add(this.lblPrecio);
             this.gbInformacion.Controls.Add(this.lblInformacion);
-            this.gbInformacion.Location = new System.Drawing.Point(0, 221);
+            this.gbInformacion.Location = new System.Drawing.Point(79, 230);
             this.gbInformacion.Name = "gbInformacion";
-            this.gbInformacion.Size = new System.Drawing.Size(338, 207);
+            this.gbInformacion.Size = new System.Drawing.Size(338, 154);
             this.gbInformacion.TabIndex = 4;
             this.gbInformacion.TabStop = false;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(10, 60);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(320, 78);
+            this.lblPrecio.TabIndex = 1;
+            this.lblPrecio.Text = resources.GetString("lblPrecio.Text");
             // 
             // lblInformacion
             // 
@@ -175,64 +178,10 @@
             this.lblInformacion.TabIndex = 0;
             this.lblInformacion.Text = "⛔ Información";
             // 
-            // lblPrecio
-            // 
-            this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(17, 54);
-            this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(125, 13);
-            this.lblPrecio.TabIndex = 1;
-            this.lblPrecio.Text = "▪︎ Precio por kilómetro: $5";
-            // 
-            // lblDescuento
-            // 
-            this.lblDescuento.AutoSize = true;
-            this.lblDescuento.Location = new System.Drawing.Point(17, 76);
-            this.lblDescuento.Name = "lblDescuento";
-            this.lblDescuento.Size = new System.Drawing.Size(92, 13);
-            this.lblDescuento.TabIndex = 2;
-            this.lblDescuento.Text = "▪︎ Descuento: 50%";
-            // 
-            // lblCondicion
-            // 
-            this.lblCondicion.AutoSize = true;
-            this.lblCondicion.Location = new System.Drawing.Point(17, 99);
-            this.lblCondicion.Name = "lblCondicion";
-            this.lblCondicion.Size = new System.Drawing.Size(319, 13);
-            this.lblCondicion.TabIndex = 3;
-            this.lblCondicion.Text = "▪︎ Condición de descuento: Distancia ≥ 100km y estancia ≥ 7 días.";
-            // 
-            // lblEstancia
-            // 
-            this.lblEstancia.AutoSize = true;
-            this.lblEstancia.Location = new System.Drawing.Point(18, 123);
-            this.lblEstancia.Name = "lblEstancia";
-            this.lblEstancia.Size = new System.Drawing.Size(154, 13);
-            this.lblEstancia.TabIndex = 4;
-            this.lblEstancia.Text = "▪︎ Días de estancia entre 1 y 30";
-            // 
-            // lblBoleto
-            // 
-            this.lblBoleto.AutoSize = true;
-            this.lblBoleto.Location = new System.Drawing.Point(18, 149);
-            this.lblBoleto.Name = "lblBoleto";
-            this.lblBoleto.Size = new System.Drawing.Size(141, 13);
-            this.lblBoleto.TabIndex = 5;
-            this.lblBoleto.Text = "▪︎ El boleto es de ida y vuelta";
-            // 
-            // lblResultado
-            // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(18, 175);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(300, 13);
-            this.lblResultado.TabIndex = 6;
-            this.lblResultado.Text = "▪︎ El resultado del cálculo se muestra en el panel de la derecha";
-            // 
             // btnLimpiar
             // 
             this.btnLimpiar.Font = new System.Drawing.Font("Sans Serif Collection", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpiar.Location = new System.Drawing.Point(8, 451);
+            this.btnLimpiar.Location = new System.Drawing.Point(87, 390);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(146, 34);
             this.btnLimpiar.TabIndex = 5;
@@ -244,18 +193,52 @@
             this.btnCalcular.BackColor = System.Drawing.Color.RoyalBlue;
             this.btnCalcular.Font = new System.Drawing.Font("Sans Serif Collection", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCalcular.ForeColor = System.Drawing.Color.White;
-            this.btnCalcular.Location = new System.Drawing.Point(175, 451);
+            this.btnCalcular.Location = new System.Drawing.Point(254, 390);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(161, 34);
             this.btnCalcular.TabIndex = 6;
             this.btnCalcular.Text = "🖩 Calcular Precio";
             this.btnCalcular.UseVisualStyleBackColor = false;
             // 
+            // gbCondicionDescuento
+            // 
+            this.gbCondicionDescuento.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gbCondicionDescuento.Controls.Add(this.lblInformacionDescuento);
+            this.gbCondicionDescuento.Controls.Add(this.lblCondicionDescuento);
+            this.gbCondicionDescuento.Location = new System.Drawing.Point(79, 440);
+            this.gbCondicionDescuento.Name = "gbCondicionDescuento";
+            this.gbCondicionDescuento.Size = new System.Drawing.Size(338, 131);
+            this.gbCondicionDescuento.TabIndex = 7;
+            this.gbCondicionDescuento.TabStop = false;
+            // 
+            // lblCondicionDescuento
+            // 
+            this.lblCondicionDescuento.AutoSize = true;
+            this.lblCondicionDescuento.Font = new System.Drawing.Font("Sans Serif Collection", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCondicionDescuento.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblCondicionDescuento.Location = new System.Drawing.Point(0, 16);
+            this.lblCondicionDescuento.Name = "lblCondicionDescuento";
+            this.lblCondicionDescuento.Size = new System.Drawing.Size(218, 44);
+            this.lblCondicionDescuento.TabIndex = 0;
+            this.lblCondicionDescuento.Text = "⛔ Condicion de descuento:";
+            this.lblCondicionDescuento.Click += new System.EventHandler(this.lblCondicionDescuento_Click);
+            // 
+            // lblInformacionDescuento
+            // 
+            this.lblInformacionDescuento.AutoSize = true;
+            this.lblInformacionDescuento.Location = new System.Drawing.Point(13, 54);
+            this.lblInformacionDescuento.Name = "lblInformacionDescuento";
+            this.lblInformacionDescuento.Size = new System.Drawing.Size(236, 39);
+            this.lblInformacionDescuento.TabIndex = 1;
+            this.lblInformacionDescuento.Text = " Para aplicar el descuento del 50% debe cumplir:\r\n▪︎ Distancia mayor o igual a 10" +
+    "0km.\r\n▪︎ Dias de estancia mayor o igual a 7 dias.\r\n";
+            // 
             // frmFerro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(851, 594);
+            this.ClientSize = new System.Drawing.Size(503, 602);
+            this.Controls.Add(this.gbCondicionDescuento);
             this.Controls.Add(this.btnCalcular);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.gbInformacion);
@@ -265,13 +248,17 @@
             this.Controls.Add(this.pbTren);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "frmFerro";
             this.Text = "Ferrocarril 🚆";
+            this.Load += new System.EventHandler(this.frmFerro_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbTren)).EndInit();
             this.gbDatos.ResumeLayout(false);
             this.gbDatos.PerformLayout();
             this.gbInformacion.ResumeLayout(false);
             this.gbInformacion.PerformLayout();
+            this.gbCondicionDescuento.ResumeLayout(false);
+            this.gbCondicionDescuento.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -286,18 +273,16 @@
         private System.Windows.Forms.Label lblDias;
         private System.Windows.Forms.Label lblDistancia;
         private System.Windows.Forms.Label lblDatos;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox mtbDias;
         private System.Windows.Forms.MaskedTextBox mtbDistancia;
         private System.Windows.Forms.GroupBox gbInformacion;
         private System.Windows.Forms.Label lblInformacion;
-        private System.Windows.Forms.Label lblResultado;
-        private System.Windows.Forms.Label lblBoleto;
-        private System.Windows.Forms.Label lblEstancia;
-        private System.Windows.Forms.Label lblCondicion;
-        private System.Windows.Forms.Label lblDescuento;
         private System.Windows.Forms.Label lblPrecio;
         private System.Windows.Forms.Button btnLimpiar;
         private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.GroupBox gbCondicionDescuento;
+        private System.Windows.Forms.Label lblCondicionDescuento;
+        private System.Windows.Forms.Label lblInformacionDescuento;
     }
 }
 
--- pryTorresFerrocarril/frmFerro.cs
+++ pryTorresFerrocarril/frmFerro.cs
@@ -21,5 +21,15 @@ namespace pryTorresFerrocarril
         {
 
         }
+
+        private void frmFerro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCondicionDescuento_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
